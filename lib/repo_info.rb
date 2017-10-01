5cff90def457417f29d9defc4b83668560903848
Soumya Ray <soumya.ray@gmail.com>
2017-10-01 13:00:19 +0800

    cache response with url keys

--- lib/repo_info.rb
+++ lib/repo_info.rb
@@ -15,8 +15,10 @@ end
 gh_response = {}
 gh_results = {}
 
-gh_response['repo'] = call_gh_url(config, gh_api_path('soumyaray/YPBT-app'))
-repo = gh_response['repo'].parse
+## GOOD REPO (HAPPY)
+repo_url = gh_api_path('soumyaray/YPBT-app')
+gh_response[repo_url] = call_gh_url(config, repo_url)
+repo = gh_response[repo_url].parse
 
 gh_results['size'] = repo['size']
 # should be 551
@@ -30,8 +32,9 @@ gh_results['git_url'] = repo['git_url']
 gh_results['contributors_url'] = repo['contributors_url']
 # "should be https://api.github.com/repos/soumyaray/YPBT-app/contributors"
 
-gh_response['contributors'] = call_gh_url(config, repo['contributors_url'])
-contributors = gh_response['contributors'].parse
+contributors_url = repo['contributors_url']
+gh_response[contributors_url] = call_gh_url(config, contributors_url)
+contributors = gh_response[contributors_url].parse
 
 gh_results['contributors'] = contributors
 contributors.count
@@ -39,6 +42,11 @@ contributors.count
 
 contributors.map { |c| c['login'] }
 # should be ["Yuan-Yu", "SOA-KunLin", "luyimin"]
+
+## BAD REPO (SAD)
+bad_repo_url = gh_api_path('soumyaray/foobar')
+gh_response[bad_repo_url] = call_gh_url(config, bad_repo_url)
+gh_response[bad_repo_url].parse   # makes sure any streaming finishes
 
 File.write('spec/fixtures/gh_response.yml', gh_response.to_yaml)
 File.write('spec/fixtures/gh_results.yml', gh_results.to_yaml)
